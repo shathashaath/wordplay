4026581ace21ed47e5a7dca134af015b32d639cb
Shatha Shaath and Shradha Pulla <shatha@>
2016-06-06 16:06:59 -0700

    Gather sentence input info and separate each word

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   $("form#content").submit(function(event) {
-    var sentence = "inputUser";
-    var res = sentence.split(" ");
+    var inputSentence = $("input#sentence").val();
+    var res = inputSentence.split(" ");
       alert(res);
 
-    // function sentence(stringToSplit, separator) {
-    // var arrayOfStrings = stringToSplit.split(separator);
+    event.preventDefault();
+
   });
 
-    event.preventDefault();
 });
